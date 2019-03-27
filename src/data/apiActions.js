8834aae12ecd6948040aa1c75c983f66bc781a61
andy <andy@Olivers-MacBook-Air.local>
2019-03-27 14:04:59 +0000

    no changes

--- src/data/apiActions.js
+++ src/data/apiActions.js
@@ -7,7 +7,7 @@ export const getExercisePlan = data => (dispatch, getState) => {
 	
 	console.log(data);	
 	// make an api call sending off the data (in the format that the api is expecting)
-	axios.get("/activity")
+	axios.get("/activity", )
 		.then( response => console.log(response))
 		.catch( error => console.log(error))
 
