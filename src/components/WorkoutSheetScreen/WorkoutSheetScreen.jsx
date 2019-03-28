dff5830f04e9cbcce31de5c353f87185af7a342c
Tat Lay <GratitudeNow@Tat.local>
2019-03-28 14:17:51 +0000

    before styling the table

--- src/components/WorkoutSheetScreen/WorkoutSheetScreen.jsx
+++ src/components/WorkoutSheetScreen/WorkoutSheetScreen.jsx
@@ -7,6 +7,8 @@ import Button from "../Button";
 
 const WorkoutSheetScreen = ({ exerciseFocus, workoutTimeMins, restTime }) => (
     <Fragment>
+    	<div class="workoutSheetScreen_wrapper">
+    	<h1 class="timemusclescreen__title">Workout with Julie</h1>
         <h1>{exerciseFocus}</h1>
         <TimeDisplay time={workoutTimeMins}>Workout Time:</TimeDisplay>
         <TimeDisplay time={restTime}>Rest Time:</TimeDisplay>
@@ -15,6 +17,7 @@ const WorkoutSheetScreen = ({ exerciseFocus, workoutTimeMins, restTime }) => (
         <Link to="/">
             <Button>Done</Button>
         </Link>
+    	</div>
     </Fragment>
 );
 
